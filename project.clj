21899b97dec0b882c327432aaf096c34ce028681
Matthias Varberg Ingesman <matthias@ingesman.dk>
2018-04-23 20:58:56 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/tools.namespace "0.2.11"]
-                 [org.clojure/tools.nrepl "0.2.12"]
-                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [org.clojure/tools.namespace "0.3.0-alpha4"]
+                 [org.clojure/tools.nrepl "0.2.13"]
+                 [org.clojure/tools.reader "1.2.2"]
                  [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jmdk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                 [neovim-client "0.1.0"]]
+                 [neovim-client "0.1.2"]]
   :main ^:skip-aot socket-repl.system
   :target-path "target/%s"
   :profiles {:uberjar {:aot [clojure.tools.logging.impl
